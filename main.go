ec7235a25feee60f9468173a5c872f79c47ca6db
gituqi123 <gituqisalam@gmail.com>
2024-06-27 09:07:13 +0330

    Update main.go
    everything OK 
    time added to log

--- main.go
+++ main.go
@@ -17,29 +17,27 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-func get_page(link string) string {
+func get_page(link string) (int, string) {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	resp, err := http.Get(link)
 	if err != nil {
-		os.Exit(-1)
+		fmt.Printf("[ERROR] can't connect to website %s\n", link)
+		return -1, "error"
 	}
 	resp.Header.Set("User-Agent", "aft")
 	defer resp.Body.Close()
-
 	buf := new(bytes.Buffer)
 	_, err = buf.ReadFrom(resp.Body)
 	if err != nil {
-		os.Exit(-1)
+		fmt.Printf("[ERROR] can't read from %s \n", link)
+		return -1, "error"
 	}
-	//fmt.Printf("client: status code: %d\n")
-	// if resp.StatusCode == 200 {
-	// 	fmt.Printf("url: %s \n", resp.Request.URL.String())
-	// }
-	return buf.String()
+	return resp.StatusCode, buf.String()
 }
 
 func alert_sound() {
-	for {
+	count := 0
+	for count < 1 {
 
 		f, err := os.Open("./peru-alert.mp3")
 		if err != nil {
@@ -64,7 +62,7 @@ func alert_sound() {
 }
 
 func remove_false_changes(web_page string) string {
-	black_list_strings := []string{"weather", "azan", "views_count", "btn3-style-gradient", "timetoday", "data-lightbox", "su-image-carousel", "wp-aparat", "", "typography_", "_StyledDynamicTypographyComponent"}
+	black_list_strings := []string{"weather", "azan", "views_count", "btn3-style-gradient", "timetoday", "data-lightbox", "su-image-carousel", "wp-aparat", "theme_token", "time", "form-actions form-wrapper", "view-dom-id-", "_blank", "data-nid"}
 	//black_list_strings := []string{"typography_", "_StyledDynamicTypographyComponent"}
 	splitted_string := strings.Split(web_page, "\n")
 	for i := 0; i < len(splitted_string); i++ {
@@ -74,44 +72,88 @@ func remove_false_changes(web_page string) string {
 				if i+1 != len(splitted_string) {
 					splitted_string[i+1] = ""
 				}
+				if i+2 < len(splitted_string) {
+					splitted_string[i+2] = ""
+				}
 			}
 		}
 	}
+	// fmt.Println(splitted_string)
 	return strings.Join(splitted_string, "\n")
 }
 
 func checker(domain string) {
+	dt := time.Now()
 	old_hash := ""
-	page := get_page(domain)
+	status_code, page := get_page(domain)
+	for status_code == -1 {
+		status_code, page = get_page(domain)
+		time.Sleep(5 * time.Second)
+	}
+
 	cleaned_string := remove_false_changes(page)
+	for status_code != 200 {
+		fmt.Printf("[alert] %s domain -> %s  , status_code: %d \n", dt.String(), domain, status_code)
+		alert_sound()
+		time.Sleep(3 * time.Second)
+	}
 	hasher := sha256.New()
 	hasher.Write([]byte(cleaned_string))
 	initial_hash := hasher.Sum(nil)
 	old_hash = hex.EncodeToString(initial_hash)
-	// fmt.Printf("%s %s \n\n\n", domain, cleaned_string)
+	// fmt.Printf("%s %s \n\n\n", domain, cleaned_string) // debug change detection
 	for {
-		hasher := sha256.New()
-		page := get_page(domain)
-		cleaned_string := remove_false_changes(page)
+		hasher = sha256.New()
+		status_code, page = get_page(domain)
+		for status_code == -1 {
+			status_code, page = get_page(domain)
+			time.Sleep(5 * time.Second)
+		}
+		for status_code != 200 {
+			fmt.Printf("[alert] %s domain -> %s  , status_code: %d \n", dt.String(), domain, status_code)
+			alert_sound()
+			time.Sleep(5 * time.Second)
+		}
+		cleaned_string = remove_false_changes(page)
 		hasher.Write([]byte(cleaned_string))
 		new_hash := hasher.Sum(nil)
 		new_hash_string := hex.EncodeToString(new_hash)
 		if old_hash != new_hash_string {
-			fmt.Printf("[alert] %s domain changed [alert]\n", domain)
-			fmt.Printf("domain: %s  old = %s , new= %s \n", domain, old_hash, new_hash_string)
-			// fmt.Printf("%s %s \n\n\n", domain, cleaned_string)
+			dt = time.Now()
+			fmt.Printf("[alert] %s  domain ->  %s changed  status_code: %d [alert]\n", dt.String(), domain, status_code)
+			// fmt.Printf("%s %s \n\n\n", domain, cleaned_string) // debug change detection
 			alert_sound()
 
 		}
-		fmt.Printf("[+] %s domain analyzed \n", domain)
+		lowercase_page := strings.ToLower(cleaned_string)
+		if strings.Contains(lowercase_page, "hack") {
+			dt = time.Now()
+			fmt.Printf("[alert] %s  domain ->  %s hack keyword detected: %s  status_code: %d [alert]\n", dt.String(), domain, "hack", status_code)
+			alert_sound()
+		} else if strings.Contains(lowercase_page, "هک شد") {
+			dt = time.Now()
+			fmt.Printf("[alert] %s  domain ->  %s hack keyword detected: %s  status_code: %d [alert]\n", dt.String(), domain, "هک", status_code)
+			alert_sound()
+		} else if strings.Contains(lowercase_page, "مرگ") {
+			dt = time.Now()
+			fmt.Printf("[alert] %s  domain ->  %s hack keyword detected: %s  status_code: %d [alert]\n", dt.String(), domain, "مرگ", status_code)
+			alert_sound()
+		} else if strings.Contains(lowercase_page, "deface") {
+			dt = time.Now()
+			fmt.Printf("[alert] %s  domain ->  %s hack keyword detected: %s  status_code: %d [alert]\n", dt.String(), domain, "deface", status_code)
+			alert_sound()
+		}
+
+		dt = time.Now()
+		fmt.Printf("[+] %s  domain -> %s analyzed \n", dt.String(), domain)
 		old_hash = hex.EncodeToString(new_hash)
 		// fmt.Printf("domain: %s hash: %s\n", domain, old_hash)
-		time.Sleep(60 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 }
 
 func main() {
 
-	go checker("google.com")
+	go checker("https://google.ir/")
 	select {}
 }
